c1994d0ec96c9b63222cd37fbe135afc17b626ef
Darroyo60 <130765508+Darroyo60@users.noreply.github.com>
2023-09-29 18:08:34 -0500

    primer commit saca datos

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import requests
 url = "https://www.deprati.com.ec/es/back-to-classics/c/c/0209A574/results?q=%3Arelevance&page=1"
 user_agent = {'User-agent': 'Mozilla/5.0'}
 
+
 # Send an HTTP GET request
 response = requests.get(url=url, headers=user_agent)
 
